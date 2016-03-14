343f83023e44044a427c86654649dcb3046d786d
wgkcoding <will.g.keith@gmail.com>
2016-03-14 17:21:33 -0400

    viewB viewHeadlines headline carousel

--- app/js/controllers/viewBCtrl.js
+++ app/js/controllers/viewBCtrl.js
@@ -12,26 +12,24 @@ angular
 			ctrl.active = 0;
 
 			ctrl.slides = [
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2014-2-11_A6.jpg",
+			{image:"../assets/img/frontPages/story-1.jpg",
 			id:0},
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2014-14-12_A7.jpg",
+			{image:"../assets/img/frontPages/story-2.jpg",
 			id:1},
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2015-3-1_GT1.jpg",
+			{image:"../assets/img/frontPages/story-3.jpg",
 			id:2},
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2015-22-10_A1.jpg",
+			{image:"../assets/img/frontPages/story-4.jpg",
 			id:3},
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2016-3-2_A1.jpg",
+			{image:"../assets/img/frontPages/story-5.jpg",
 			id:4},
-			{image:"../assets/img/frontPages/STAR_ONT_ON_2016-26-1_A1.jpg",
+			{image:"../assets/img/frontPages/story-6.jpg",
 			id:5},
-			{image:"../assets/img/frontPages/STAR_ONT_Replate_ON_2013-15-12_A1.jpg",
+			{image:"../assets/img/frontPages/story-7.jpg",
 			id:6},
-			{image:"../assets/img/frontPages/STAR_ONT_Replate_ON_2015-5-12_A1.jpg",
+			{image:"../assets/img/frontPages/story-8.jpg",
 			id:7},
-			{image:"../assets/img/frontPages/STAR_ONT_Replate_ON_2015-5-10_GT1.jpg",
-			id:8},
-			{image:"../assets/img/frontPages/STAR_ONT_Replate_ON_2016-26-1_A4.jpg",
-			id:9}
+			{image:"../assets/img/frontPages/story-9.jpg",
+			id:8}
 			];
 
 			console.log(ctrl.testMsg);
