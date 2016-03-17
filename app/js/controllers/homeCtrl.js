e8f61cc557c1ab7efb97446630f8ff0a55f9c308
wgkcoding <will.g.keith@gmail.com>
2016-03-17 06:11:37 -0400

    master commit

--- app/js/controllers/homeCtrl.js
+++ app/js/controllers/homeCtrl.js
@@ -2,12 +2,19 @@ angular
 	.module('app')
 	.controller('homeCtrl', homeCtrl);
 
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	// function (scope, el, atrrs) {
+	// 	el.bind('$destroy', function(){
+	// 		var twitterScriptEl = angular.element('twiiter-wjs');
+	// 		twitterScriptEl.remove();
+	// 	});
 
-	twttr.ready(function (twttr) {
-		twttr.widgets.load();
-		// twttr.events.bind('click', function (event) {alert('yes'); });
-	});
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	// }	
+
+	// TwitterService.load();
+	// function run(){
+	// 	twttr.widgets.load();
+	// };
 
 	function homeCtrl(dbService) {
 		var ctrl = this;
